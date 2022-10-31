4e2eb94ea623ee70327b3aa46740c16fcccdf38b
romerocam <romerocam@hotmail.com>
2022-10-31 18:45:12 -0300

    required inputs ok

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm("maykprjn");
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
   return (
     <div className="max-w-[1240px] m-auto p-4 h-screen">
       <h1 className="text-2xl font-bold text-center p-4">Tu consulta aquí</h1>
@@ -20,44 +15,35 @@ const Contact = () => {
             name="name"
             type="text"
             placeholder="Nombre"
+            required="true"
           />
-          <ValidationError prefix="Name" field="name" errors={state.errors} />
+
           <input
             className="border shadow-lg p-3"
             name="email"
             type="email"
             placeholder="Email"
+            required="true"
           />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <input
           className="border shadow-lg p-3 w-full my-2"
           name="subject"
           type="text"
           placeholder="Asunto"
+          required="true"
         />
-        <ValidationError
-          prefix="Subjectl"
-          field="subject"
-          errors={state.errors}
-        />
+
         <textarea
           className="border shadow-lg p-3 w-full"
           name="message"
           cols="30"
           rows="10"
           placeholder="Mensaje"
+          required="true"
         ></textarea>
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button
-          type="submit"
-          disabled={state.submitting}
-          className="border shadow-lg p-3 w-full mt-2"
-        >
+
+        <button type="submit" className="border shadow-lg p-3 w-full mt-2">
           Enviar
         </button>
       </form>
