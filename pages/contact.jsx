d2817282eec5533cde9737b2878f7bf18d1154ee
Juan Camilo Romero <101602875+romerocam@users.noreply.github.com>
2024-01-22 08:30:42 -0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,7 +5,7 @@ import Main from '../components/Main'
 const contact = () => {
   return (
     <div>
-        <Main heading='Contacto' message='Envíanos tu consulta completando el formulario' />
+        <Main heading='Contacto' message='Envíanos tu consulta completando el formulario de abajo' />
         <Contact />
     </div>
   )
