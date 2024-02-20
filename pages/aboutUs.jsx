7806fcbdd5bebd10e6bbe1223598780362ff62bb
Juan Camilo Romero <101602875+romerocam@users.noreply.github.com>
2024-02-20 11:54:17 -0300

    Update aboutUs.jsx

--- pages/aboutUs.jsx
+++ pages/aboutUs.jsx
@@ -5,7 +5,7 @@ import Portfolio from '../components/Portfolio'
 const aboutUs = () => {
   return (
     <div>
-        <Main heading='Nosotros' message='Somos Juan Y Marina'/>
+        <Main heading='Nosotros' message='Somos Juan Y Marina'   />
         <Portfolio />
     </div>
   )
